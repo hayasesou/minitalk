89358659ff20216529827280b0b8cf1c6c894a81
Hayase Fukushi <hfukushi@c1r14s5.42tokyo.jp>
2023-08-23 08:53:52 +0900

    fix error of split in libft

--- client.c
+++ client.c
@@ -1,20 +1,19 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
+#include "libft.h"
 
-void send_signal(unsigned char c,pid_t pid)
+void	send_signal(unsigned char c,pid_t pid)
 {
-	int digit;
+	int	digit;
 
 	digit = 7;
-
 	while (digit >= 0)
 	{
 		if ((c >> digit) & 0x01)
-			kill(pid,SIGUSR1);
+			kill(pid, SIGUSR1);
 		else
-		kill(pid,SIGUSR2);
+			kill(pid, SIGUSR2);
 		digit--;
 		usleep(150);
 	}
@@ -22,9 +21,10 @@ void send_signal(unsigned char c,pid_t pid)
 	digit = 7;
 }
 
-void send_char(char *str,pid_t pid)
+void	send_char(char *str, pid_t pid)
 {
-	unsigned char c;
+	unsigned char	c;
+
 	while (*str != '\0')
 	{
 		c = (unsigned char)(*str);
@@ -35,14 +35,19 @@ void send_char(char *str,pid_t pid)
 
 int	main(int ac, char **av)
 {
-	pid_t pid;
+	pid_t	pid;
 
-	pid = (pid_t)atoi(av[1]);
-	if(ac != 3)
+	pid = (pid_t)ft_atoi(av[1]);
+	if (pid < 100 || pid > 99998)
 	{
-		write(1,"error. please input : ./client PID XXXX\n",40);
-		return(0);
+		write (1, "error. invalid PID\n", 19);
+		return (1);
+	}
+	if (ac != 3)
+	{
+		write (1, "error. please input : ./client PID XXXX\n", 40);
+		return (1);
 	}
 	send_char(av[2],pid);
-	return(0);
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,7 +1,7 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include "libft.h"
 #include <libc.h>
 
 void handler(int signum, siginfo_t *client, void *con)
@@ -15,8 +15,6 @@ void handler(int signum, siginfo_t *client, void *con)
 	{
 	if (signum == SIGUSR1)
 		c = (1 << bit_counter) | c;
-	// else
-	// 	c = (1 << bit_counter) & 0;
 	if(bit_counter == 0)
 	{
 		write(1,&c,1);
@@ -28,32 +26,15 @@ void handler(int signum, siginfo_t *client, void *con)
 	}
 }
 
-// int get_byte(char *c)
-// {
-// 	int byte ;
-// 	int digit;
-
-// 	digit = 7;
-// 	byte = 0;
-// 	if(!(*c & (1 << 7)))
-// 		return(1);
-// 	while(*c & (1 << digit))
-// 	{
-// 		byte++;
-// 		digit--;
-// 	}
-// 	return(byte);
-// }
-
-
-
 int main ()
 {
 	pid_t	pid;
 	struct sigaction sa;
 
 	pid = getpid();
-	printf("pid = [%d]\n",pid);
+	write(1,"pid = [",7);
+	ft_putnbr_fd(pid,1);
+	write(1,"]\n",2);
 
 	memset(&sa,0,sizeof(struct sigaction));
 	sigemptyset(&sa.sa_mask);
